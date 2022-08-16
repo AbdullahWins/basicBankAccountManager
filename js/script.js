145392a5cc408beb8eb9496ca1e8e4bd4fe29eec
AbdullahWins <noyononline@gmail.com>
2022-08-16 23:31:35 +0600

    fixed router

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.getElementById('btn-submit').addEventListener('click' , function () {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     if (email === 'a' && password === '1') {
-        window.location.href = '../html/basicBank.html';
+        window.location.href = 'html/basicBank.html';
     }
     else {
         alert('Wrong Info');
